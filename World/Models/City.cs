a9b9c7f38d55ff90515918a2abc95a90d5360ab9
Victoria Oh <ohvictori@gmail.com>
2018-09-17 13:49:10 -0700

    Create City.cs file. Create database object city and set-up to retrieve from database.

---
+++ World/Models/City.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using World;
+using System;
+
+namespace World.Models
+{
+    public class City
+    {
+        private int _id;
+        private string _cityName;
+
+        public City(string CityName, int Id=0)
+        {
+            _id = Id;
+            _cityName = CityName;
+        }
+
+        public int GetId() => _id;
+        public void SetID(int Id) => _id = Id;
+
+        public string GetCityName() => _cityName;
+        public void SetCityName() => _cityName = CityName;
+
+        public static List<City> GetAll()
+        {
+            List<City> allCities = new List<City>{};
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM city;"
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while(rdr.Read())
+            {
+                int cityId = rdr.GetInt32(0);
+                string cityName = rdr.GetString(1);
+                City newCity = new City(cityName, cityId);
+                allCities.Add(newCity);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allCities;
+        }
+    }
+}
