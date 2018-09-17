b5abbed4f06f3fb356451fa9a2ae20a1b9a18d81
Victoria Oh <ohvictori@gmail.com>
2018-09-17 12:50:18 -0700

    Add dependencies.

--- World/Startup.cs
+++ World/Startup.cs
@@ -38,4 +38,8 @@ namespace World
             });
         }
     }
+    public static class DBConfiguration
+        {
+            public static string ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=todo;";
+        }
 }
